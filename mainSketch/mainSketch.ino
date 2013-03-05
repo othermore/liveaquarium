793e723c6f74680f5ab250d607357dd676feab23
Antonio Morales García <antonio@iuavs.com>
2013-03-05 22:04:48 +0100

    Reemplazada librería RTC por una mejor. Sistema de Dimming funcionando

--- mainSketch/mainSketch.ino
+++ mainSketch/mainSketch.ino
@@ -4,27 +4,43 @@
 #include <Wire.h>
 #include <SD.h>
 
-#include "RealTimeClockDS1307.h"
-#include "webServer.h"
+#include "RTClib.h"
+RTC_DS1307 RTC;
 
+#include "dimmer.h"
+Dimmer dim1(13, 10, 240, 20, 9*60*60, 21*60*60 );
+
+#include "webServer.h"
 char settingsFileName[] = "settings.txt";
 WebServer webServer(true, 60000,10);
 #include "webServerActions.h"
 
 long prev_millis;
-char formatted[] = "00-00-00 00:00:00x";
-void setup() {
 
-  Serial.begin(9600);
+
+void setup() {
+  
+  Serial.begin(57600);
+  Wire.begin();
+  RTC.begin(); 
+  if (! RTC.isrunning()) {
+    Serial.println("RTC is NOT running!");
+  }
   if (!SD.begin(4)) {
       Serial.println("SD initialization failed!");
       return;
   }
   Serial.println("SD initialization done.");
 
+  dim1.setup();
+
   Settings::loadSettings(settingsFileName);
   Settings::printSettings();
   bool changed = false;
+  if(Settings::getSettingType("ledLevel") == Settings::UNKNOWN)
+  {
+    Settings::setIntSetting("ledLevel", 0);
+  }
   if(!Settings::getStringSetting("wifi_ssid"))
   {
     changed = true;
@@ -50,21 +66,40 @@ void setup() {
   webServer.registerFunction(wsa_clock_keyword, wsa_clock);
   webServer.registerFunction(wsa_settings_keyword, wsa_settings);
   webServer.registerFunction(wsa_settings_update_keyword, wsa_settings_update);
+  webServer.registerFunction(wsa_dim_mode_keyword, wsa_dim_mode);
   Serial.println("Web server actions registered.");
-	prev_millis = millis();      
+	prev_millis = millis();  
+  
 }
 
 void loop() {
 	// put your main code here, to run repeatedly: 
 	webServer.loop();
+  dim1.loop();
 	if(millis() - prev_millis > 10000)
 	{
-                Serial.print("*");
-		RTC.readClock();
-		RTC.getFormatted(formatted);
+    Serial.print("*");
+		DateTime now = RTC.now();
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    
+    Serial.print(" since midnight 1/1/1970 = ");
+    Serial.print(now.unixtime());
+    Serial.print("s = ");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println("d");
 		prev_millis = millis();
-                Serial.print("-");
-		Serial.print(formatted);
+    Serial.print("-");
 		Serial.println();
   	}
 }
--- mainSketch/webServerActions.h
+++ mainSketch/webServerActions.h
@@ -14,11 +14,22 @@ void wsa_clock(WiFiClient* client)
 {
     WebServer::sendHtmlHeaders(client);
     WebServer::finishHeaders(client);
-    char time[] = "00-00-00 00:00:00x";
-    RTC.readClock();
-    RTC.getFormatted(time);
+
     client->print("<html><head><title>Clock</title></head><body>");
-    client->print(time);
+    DateTime now = RTC.now();
+    client->print(now.year(), DEC);
+    client->print('/');
+    client->print(now.month(), DEC);
+    client->print('/');
+    client->print(now.day(), DEC);
+    client->print(' ');
+    client->print(now.hour(), DEC);
+    client->print(':');
+    client->print(now.minute(), DEC);
+    client->print(':');
+    client->print(now.second(), DEC);
+
+
     client->print("</body></html>");
 }
 
@@ -39,6 +50,91 @@ void wsa_settings_update(WiFiClient* client)
     Settings::loadSettings(client);
     Settings::saveSettings(settingsFileName);
     wsa_settings(client);
+}
+
+char* wsa_dim_mode_keyword="PUT /dim HTTP/1.1";
+void wsa_dim_mode(WiFiClient* client)
+{
+    WebServer::discardRemainingHeaders(client);
+    // Leemos el comando que nos mandan
+    char comando [20];
+    int leido = 0;
+    while(client->available() && leido < 10)
+    {
+        comando[leido] = client->read();
+        leido++;
+        Serial.write(comando[leido]);
+    }
+    comando[leido] = '\0';
+    int cmd = atoi(comando);
+
+    Serial.print("Leido: ");
+    Serial.print(leido);
+    Serial.print(" Comando: ");
+
+    Serial.print(comando);
+    Serial.print(" --> ");
+    Serial.println(cmd);
+
+    if(cmd == 0)
+    {
+        dim1.setOff();
+        Serial.println("Set OFF");
+    }
+    if(cmd == 1)
+    {
+        dim1.setOn();
+        Serial.println("Set ON");
+    }
+    if(cmd == 2)
+    {
+        dim1.setOffWithDimming();
+        Serial.println("Dim to OFF");
+    }
+    if(cmd == 3)
+    {
+        dim1.setOnWithDimming();
+        Serial.println("Dim to ON");
+    }
+    if(cmd == 4)
+    {
+        dim1.setOff();
+        Serial.println("Set OFF");
+    }
+    if(cmd == 5)
+    {
+        dim1.setOn();
+        Serial.println("Set ON");
+    }
+    if(cmd == 6)
+    {
+        dim1.startDimmingOffAndAuto();
+        Serial.println("Dim to OFF and AUTO");
+    }
+    if(cmd == 7)
+    {
+        dim1.startDimmingOnAndAuto();
+        Serial.println("Dim to ON and AUTO");
+    }
+    if(cmd == 8)
+    {
+        dim1.setAuto();
+        DateTime now = RTC.now();
+        dim1.day_auto_off = (now.hour()*60+now.minute())*60+now.second()+20;
+        dim1.day_auto_on = (now.hour()*60+now.minute())*60+now.second()+80;
+        Serial.println("Auto to OFF in 20 secconds and ON in 80");
+    }
+    if(cmd == 9)
+    {
+        dim1.setAuto();
+        DateTime now = RTC.now();
+        dim1.day_auto_on = (now.hour()*60+now.minute())*60+now.second()+20;
+        dim1.day_auto_off = (now.hour()*60+now.minute())*60+now.second()+80;
+        Serial.println("Auto to ON in 20 secconds and OFF in 80");
+    }
+
+
+    
 }
 
 
